ced28cac3877527efa022421b463888b0c929ee5
hydrogenbond68 <hydrogenbond68@gmail.com>
2024-08-09 09:49:32 +0300

    first commit

---
+++ core/src/components/Dashboard/Admin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Admin = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+    </div>
+  );
+};
+
+export default Admin;
