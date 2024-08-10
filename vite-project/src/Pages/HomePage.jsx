06c51906f529670656b08be919444bcb14a2291a
jessetfb <langatjesse@gmail.com>
2024-08-10 11:06:13 +0300

    intial commit

---
+++ vite-project/src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Core from '../components/Cores.jsx'
+import NavbarComponent from '../components/Navbar.jsx'
+function Home(){
+    return (
+        <>
+        <NavbarComponent/>
+        <Core/>
+        </>
+    );
+}
+export default Home
