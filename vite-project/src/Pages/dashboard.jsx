90127c46bf8e167e2103bc0171aa776541ed35c4
Kelvin <frednjogu22@gmail.com>
2024-08-12 15:15:13 +0300

    added user dashboard

--- vite-project/src/Pages/dashboard.jsx
+++ vite-project/src/Pages/dashboard.jsx
@@ -1,12 +1,13 @@
 // src/Pages/dashboard.jsx
-
 import React from "react";
-import AdminDashboard from "../components/admin"; // Importing AdminDashboard from the components folder
+import AdminDashboard from "../components/admin"; // Import AdminDashboard from the components folder
+import UserProfile from "../components/user"; // Import UserProfile from the components folder
 
 const Dashboard = () => {
   return (
     <div>
       <AdminDashboard />
+      <UserProfile /> {/* Add UserProfile component */}
       {/* Add other components or layout elements as needed */}
     </div>
   );
