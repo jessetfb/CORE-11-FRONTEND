bc5aa2a44b5b92f327a9a650040326708336fd99
jessetfb <langatjesse@gmail.com>
2024-08-12 14:36:32 +0300

    intial commit

---
+++ vite-project/src/Pages/dashboard.jsx
@@ -0,0 +1,15 @@
+// src/Pages/dashboard.jsx
+
+import React from "react";
+import AdminDashboard from "../components/admin"; // Importing AdminDashboard from the components folder
+
+const Dashboard = () => {
+  return (
+    <div>
+      <AdminDashboard />
+      {/* Add other components or layout elements as needed */}
+    </div>
+  );
+};
+
+export default Dashboard;
